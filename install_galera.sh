5fa636f6272005c3b914f23574f2094afcca6a4c
aryonp <1676948+aryonp@users.noreply.github.com>
2018-06-12 15:09:18 +0700

    Update install_galera.sh

--- install_galera.sh
+++ install_galera.sh
@@ -6,7 +6,7 @@ DES_PATH=
 #add repo and install
 sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
 sudo add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://ftp.utexas.edu/mariadb/repo/10.1/ubuntu xenial main'
-sudo apt update && sudo apt upgrade -y && sudo apt install -y mariadb-server rsync -y
+sudo apt update && sudo apt upgrade -y && sudo apt install -y mariadb-server rsync
 sudo mysql_secure_installation
 
 #link to backup DB
